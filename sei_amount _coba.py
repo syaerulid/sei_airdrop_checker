f65c0ac8330bbc36ccf5359a433ee5a91d445441
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-08-16 15:25:58 +0700

    Add files via upload

---
+++ sei_amount _coba.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import streamlit as st
+import re
+import requests
+
+def check_eligibility(address):
+    eligible_check = f"https://incentivized-testnet.seinetwork.io/check-eligibility?seiAddress={address}"
+    response = requests.get(eligible_check)
+    response_json = response.json()
+    return response_json
+    
+def sei_amount(number_usei):
+    useiexponent = 6
+    sei_amount = number_usei / (10 ** useiexponent)
+    return sei_amount
+
+def main():
+    st.title("SEI Amount Checker")
+    address = st.text_input('Enter your SEI address:')
+    if address and address.startswith('sei'):
+        if st.button("Check Eligibility"):
+            response_json = check_eligibility(address)
+            
+            if response_json.get('message'):
+                st.write('Currently SEI Server is Down, check again later')
+            else:
+                if response_json.get("eligible").lower() is True:
+                    eligibleAmount = response_json.get("eligibleAmount")
+                    st.write("Eligible Amount (usei):", eligibleAmount)
+
+                    number_usei = int(re.search(r'\d+', eligibleAmount).group())
+                    st.write("Numeric Amount (usei):", number_usei)
+
+                    sei = sei_amount(number_usei)
+                    st.write("Eligible Amount (sei):", sei)
+                else:
+                    st.write("You are not Ahoy.")
+    else:
+        st.write("Invalid address or network.")
+
+if __name__ == "__main__":
+    main()
+
+
+# In[6]:
+
+
+check_eligibility('sei18dg6pcexurhw0mr9wh7uvsecfs5n805j5cx6f2')
+
+
+# In[ ]:
+
+
+
+
