3b7000d4a3e39cd5ab02c761f2173230d5e81d5d
Erul - Data Enthusiast <119069839+syaerulid@users.noreply.github.com>
2023-08-16 15:00:53 +0700

    Update sei_amount.py

--- sei_amount.py
+++ sei_amount.py
@@ -1,41 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import streamlit as st
 import re
 import requests
-
-
-# In[2]:
-
 
 def check_eligibility(address):
     eligible_check = f"https://incentivized-testnet.seinetwork.io/check-eligibility?seiAddress={address}"
     response = requests.get(eligible_check)
     response_json = response.json()
     return response_json
-
-
-# In[8]:
-
-
-check_eligibility("sei1uvt5zfpug3cwl6644gz2tfs5rfuk8hvrlkegzd")
-
-
-# In[3]:
-
-
+    
 def sei_amount(number_usei):
     useiexponent = 6
     sei_amount = number_usei / (10 ** useiexponent)
     return sei_amount
-
-
-# In[9]:
-
 
 def main():
     st.title("SEI Amount Checker")
@@ -47,7 +26,7 @@ def main():
             if 'Internal server error' in response_json.get('message'):
                 st.write('Currently SEI Server is Down, check again later')
             else:
-                if response_json.get("eligible") is True:
+                if response_json.get("eligible").lower() == "true":
                     eligibleAmount = response_json.get("eligibleAmount")
                     st.write("Eligible Amount (usei):", eligibleAmount)
 
@@ -63,8 +42,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# In[ ]:
 
 
 
